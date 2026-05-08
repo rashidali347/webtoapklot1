c63b2d0101a80c93519e802349693035c2a6e6cf
rashidali347 <rashidali47083490@gmail.com>
2026-05-08 04:28:14 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation(androidx.appcompat:appcompat:1.7.0)
     
     // Added dependencies
     implementation(libs.androidx.swiperefreshlayout)
